73bd6bb7b606a05e60903dc45a8aaca520536cfc
Ryan Achten <ryanachten@gmail.com>
2018-05-17 20:34:47 +1200

    Remove unrequired assets

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
-import Header from '../components/Header';
 
 
 const AppRouter = () => (
   <BrowserRouter>
     <div id="router--container">
-      <Header />
       <Switch>
         <Route path="/" component={HomePage} exact={true} />
         <Route component={NotFoundPage} />
